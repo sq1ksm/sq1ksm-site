00f795d1e814c773fceb1b8dfb0deddd290b6ae7
sq1ksm <slawera@gmail.com>
2024-07-05 20:09:33 +0200

    SOKOL

--- src/routes/(podstrony)/inne/+page.svelte
+++ src/routes/(podstrony)/inne/+page.svelte
@@ -2,6 +2,7 @@
 	<!-- tresc-->
 	<article>
 		<h1>Wszystk to co nie znalazło się gdzie indziej</h1>
+		<h2>Instrukcja - T1 Elecaft</h2>
 		<a href="/zdjecia/inne/t1_elecraft_pl.pdf">
 			<img
 				height="256"
@@ -15,14 +16,33 @@
 		<p>
 			Do pobrania:<br />
 			<br />
-			Tłumaczenie polskie instrukcji świetnej skrzynki antenowej QRP - T1 firmy Elecaft.<br />
+			Polskie tłumaczenie instrukcji świetnej skrzynki antenowej QRP - T1 firmy Elecaft.<br />
 			Pozwoliłem sobie przetłumaczyć instrukcję z języka angielskiego na polski, by w pełni i wygodnie
 			korzystać z wszystkich funkcji skrzynki antenowej.<br /> Plik:<br />
 			t1_elecraft_pl.pdf Adobe Acrobat Document 382.1KB
 			<br /><br /><br /><br /><br /><br /><br /><br />
 		</p>
 		<hr />
-		<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br
-		/><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+	</article>
+	<article>
+		<h2>Instrucja obsługi Radia SOKOL</h2>
+		<a href="/zdjecia/inne/sokol_pl.pdf">
+			<img
+				height="136"
+				width="256"
+				src="/zdjecia/inne/sokol.webp"
+				alt=" T1 firmy Elecaft"
+				title="Stacja"
+				style="margin:10px 0px 0px 10px;float:right"
+			/></a
+		>
+		<p>
+			Do pobrania przetłumaczona instrukcja obsługi przenośnego radia Sokół produkcji CCCP.<br />
+			Jak napisano w p.3 Przed włączeniem odbiornika przeczytaj uważnie niniejszą instrukcję obsługi.
+			Hi, hi.<br />
+			No cóż rosyjski nie jest językiem wszystkim na razie znanym, więc narodziła się potrzeba przetłumaczenia.
+		</p>
+		<br />
+		<hr />
 	</article>
 </div>
